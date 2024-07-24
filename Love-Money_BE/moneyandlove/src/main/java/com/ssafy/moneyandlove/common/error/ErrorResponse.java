105104bafd8ffee88a1fe6c76fb50fd15d945a2f
정지환 <64758861+stophwan@users.noreply.github.com>
2024-07-24 14:20:31 +0900

    feat: ErrorType enum 정의 및 ErrorResponse 설정

---
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/common/error/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.ssafy.moneyandlove.common.error;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+	private final String code;
+	private final String message;
+
+	public static ErrorResponse of(ErrorType errorType) {
+		return ErrorResponse
+			.builder()
+			.code(errorType.getCode())
+			.message(errorType.getMessage())
+			.build();
+	}
+}
