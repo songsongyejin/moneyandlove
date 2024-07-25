3ed479bf9125a6edfc68ce8ad7d82ca52f344d77
Lee9Bin <imagen33@daum.net>
2024-07-25 22:07:22 +0900

    feat: WebSocketEventListener 웹 소켓 연결 리스너 구현

---
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/chat/config/WebSocketEventListener.java
@@ -0,0 +1,28 @@
+package com.ssafy.moneyandlove.chat.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@Slf4j
+public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+        log.info("헤더 정보: {}", headerAccessor);
+        log.info("새로운 WebSocket 연결이 열렸습니다. 세션 ID: {}", sessionId);
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+        log.info("WebSocket 연결이 닫혔습니다. 세션 ID: {}", sessionId);
+    }
+}
