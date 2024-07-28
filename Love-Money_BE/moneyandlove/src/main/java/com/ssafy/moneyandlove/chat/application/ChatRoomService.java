de3c63a4bcba1db382ebb914aec20c1c6a86ef42
Lee9Bin <imagen33@daum.net>
2024-07-28 21:58:16 +0900

    feat: 채팅방 저장 구현중

--- Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/chat/application/ChatRoomService.java
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/chat/application/ChatRoomService.java
@@ -1,10 +1,13 @@
 package com.ssafy.moneyandlove.chat.application;
 
+import com.ssafy.moneyandlove.chat.domain.ChatRoom;
 import com.ssafy.moneyandlove.chat.dto.ChatRoomIdResponse;
 import com.ssafy.moneyandlove.chat.dto.CreateChatRoomRequest;
 import com.ssafy.moneyandlove.chat.repository.ChatRoomRepository;
 import com.ssafy.moneyandlove.common.error.ErrorType;
 import com.ssafy.moneyandlove.common.exception.MoneyAndLoveException;
+import com.ssafy.moneyandlove.user.domain.User;
+import com.ssafy.moneyandlove.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
+    private final UserRepository userRepository;
 
     public ChatRoomIdResponse findByFromUserIdAndToUserId(Long fromUserId, Long toUserID) {
         return ChatRoomIdResponse
@@ -26,6 +30,11 @@ public class ChatRoomService {
     }
 
     public ChatRoomIdResponse save(CreateChatRoomRequest createChatRoomRequest) {
-        return null;
+        User fromUser = userRepository.findById(createChatRoomRequest.getFromUserId()).orElseThrow();
+        User toUser = userRepository.findById(createChatRoomRequest.getToUserId()).orElseThrow();
+        ChatRoom build = ChatRoom.builder().roomId(createChatRoomRequest.getRoomId()).fromUser(fromUser).toUser(toUser).build();
+        ChatRoom save = chatRoomRepository.save(build);
+
+        return ChatRoomIdResponse.from(save.getRoomId());
     }
 }
