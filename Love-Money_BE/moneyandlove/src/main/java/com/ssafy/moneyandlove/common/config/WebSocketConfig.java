28143ba916931a4b4837c2e6775dfdbf87391bc2
Lee9Bin <imagen33@daum.net>
2024-07-29 14:35:51 +0900

    chore: WebSocket 설정 구성

--- Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/common/config/WebSocketConfig.java
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/common/config/WebSocketConfig.java
@@ -1,13 +1,29 @@
 package com.ssafy.moneyandlove.common.config;
 
+import com.ssafy.moneyandlove.common.annotation.LoginUser;
+import com.ssafy.moneyandlove.common.resolver.ChattingUserArgumentResolver;
+import com.ssafy.moneyandlove.user.domain.User;
+import jakarta.annotation.Nullable;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.security.Principal;
+import java.util.List;
+
 @Configuration
 //웹소켓 활성화 어노테이션
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
@@ -28,4 +44,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 			.withSockJS();
 	}
 
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(chattingUserArgumentResolver());
+	}
+
+	HandlerMethodArgumentResolver chattingUserArgumentResolver() {
+		return new ChattingUserArgumentResolver();
+	}
 }
