ce9dc391238a26891753fd7dcd1449afde8601d7
Lee9Bin <imagen33@daum.net>
2024-07-30 17:33:57 +0900

    feat: 회원가입 및 회원 정보 기능

--- Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/user/controller/UserController.java
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/user/controller/UserController.java
@@ -1,85 +1,45 @@
 package com.ssafy.moneyandlove.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
+import com.ssafy.moneyandlove.user.dto.KakaoAccount;
+import com.ssafy.moneyandlove.user.dto.KakaoToken;
+import com.ssafy.moneyandlove.user.dto.SignUpRequest;
+import com.ssafy.moneyandlove.user.dto.SignUpResponse;
+import com.ssafy.moneyandlove.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
-    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
-    private String clientId;
+    private final UserService userService;
 
-    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
-    private String clientSecret;
-
-    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
-    private String redirectUri;
-
-    @GetMapping("/login/oauth2")
-    public ResponseEntity<String> kakaoCallback(@RequestParam("code") String code) {
+    @GetMapping("/login/oauth2/callback")
+    public ResponseEntity<?> kakaoCallback(@RequestParam("code") String code) {
         log.info("code is {}", code);
-        // 1. 액세스 토큰 얻기
-        String accessToken = getAccessToken(code);
-        log.info("accessToken is {}", accessToken);
-        // 2. 사용자 정보 얻기
-        return ResponseEntity.ok(getUserInfo(accessToken));
+        KakaoToken kakaoAccessToken = userService.getKakaoAccessToken(code);
+        log.info("kakaoAccessToken is {}", kakaoAccessToken);
+        KakaoAccount kakaoInfo = userService.getKaKaoInfo(kakaoAccessToken);
+        log.info("kaKaoInfo is {}", kakaoInfo);
+        if(!userService.isSigned(kakaoInfo)){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(SignUpResponse.from(kakaoInfo));
+        }
+        return null;
     }
 
-    private String getAccessToken(String code) {
-        RestTemplate rt = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "authorization_code");
-        params.add("client_id", clientId);
-        params.add("redirect_uri", redirectUri);
-        params.add("code", code);
-        params.add("client_secret", clientSecret);
-
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
-                new HttpEntity<>(params, headers);
-
-        ResponseEntity<Map> response = rt.exchange(
-                "https://kauth.kakao.com/oauth/token",
-                HttpMethod.POST,
-                kakaoTokenRequest,
-                Map.class
-        );
-
-        return (String) response.getBody().get("access_token");
-    }
-    //https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=5f5c8c06e4b41de351c85a775ab50b40&redirect_uri=http://localhost:3000/login/oauth2/callback
-    private String getUserInfo(String accessToken) {
-        RestTemplate rt = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + accessToken);
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest =
-                new HttpEntity<>(headers);
-
-        ResponseEntity<Map> response = rt.exchange(
-                "https://kapi.kakao.com/v2/user/me",
-                HttpMethod.POST,
-                kakaoProfileRequest,
-                Map.class
-        );
-
-        return response.getBody().toString();
+    @PostMapping("/sign")
+    public ResponseEntity<?> sign(@RequestBody SignUpRequest signUpRequest){
+        userService.save(signUpRequest);
+        return ResponseEntity.ok("ok");
     }
 }
