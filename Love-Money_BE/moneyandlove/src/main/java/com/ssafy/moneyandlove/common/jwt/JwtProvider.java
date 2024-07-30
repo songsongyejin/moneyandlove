41b4553e94e10e7a66f73efdce47d63950100e4c
Lee9Bin <imagen33@daum.net>
2024-07-30 17:51:18 +0900

    feat: jwt 구현중

--- Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/common/jwt/JwtProvider.java
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/common/jwt/JwtProvider.java
@@ -1,7 +1,17 @@
 package com.ssafy.moneyandlove.common.jwt;
 
+import java.time.Duration;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import com.ssafy.moneyandlove.user.domain.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtProvider {
@@ -9,12 +19,32 @@ public class JwtProvider {
 	@Value("${jwt.secret}")
 	private String secret;
 
-	// public String generateToken(User user) {
-	// 	Date now = new Date();
-	// 	return Jwts.builder()
-	// 		.setExpiration(LocalDate.now().plus(100))
-	// 		.setClaims("id", user.getId())
-	// 		.signWith(SignatureAlgorithm.HS256, secret)
-	// 		.compact();
-	// }
+	public String generateToken(User user) {
+		Date now = new Date();
+		return Jwts.builder()
+			.setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+			.setIssuedAt(now)
+			.setExpiration(new Date(now.getTime() + Duration.ofHours(2).toMillis()))
+			.setSubject(user.getEmail())
+			.claim("id", user.getId())
+			.signWith(SignatureAlgorithm.HS256, secret)
+			.compact();
+	}
+
+	public Long getUserId(String token) {
+		Claims claims = validateToken(token);
+		return claims.get("id", Long.class);
+	}
+
+	public Claims validateToken(String token) {
+		try {
+			return Jwts.parser()
+				.setSigningKey(secret)
+				.parseClaimsJws(token)
+				.getBody();
+		} catch (Exception e) {
+			throw new IllegalArgumentException();
+		}
+	}
+
 }
