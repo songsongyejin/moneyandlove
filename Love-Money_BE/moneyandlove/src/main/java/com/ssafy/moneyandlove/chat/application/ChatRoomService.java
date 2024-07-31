8af2b2759e043b3306a34c19c2911a8dcfd8f4cd
Lee9Bin <imagen33@daum.net>
2024-07-31 22:21:42 +0900

    refactor: 변환 메서드 dto 객체로 이동

--- Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/chat/application/ChatRoomService.java
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/chat/application/ChatRoomService.java
@@ -1,5 +1,9 @@
 package com.ssafy.moneyandlove.chat.application;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
 import com.ssafy.moneyandlove.chat.domain.ChatRoom;
 import com.ssafy.moneyandlove.chat.dto.ChatRoomIdResponse;
 import com.ssafy.moneyandlove.chat.dto.CreateChatRoomRequest;
@@ -8,11 +12,9 @@ import com.ssafy.moneyandlove.common.error.ErrorType;
 import com.ssafy.moneyandlove.common.exception.MoneyAndLoveException;
 import com.ssafy.moneyandlove.user.domain.User;
 import com.ssafy.moneyandlove.user.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,13 +26,13 @@ public class ChatRoomService {
 
     public ChatRoomIdResponse findByFromUserIdAndToUserId(Long fromUserId, Long toUserID) {
         Optional<ChatRoom> chatRoom = chatRoomRepository.findChatRoomByUsers(fromUserId, toUserID);
-        return ChatRoomIdResponse.from(chatRoom.orElseThrow(() -> new MoneyAndLoveException(ErrorType.CHATROOM_NOT_FOUND)).getId());
+        return ChatRoomIdResponse.from(chatRoom.orElseThrow(() -> new MoneyAndLoveException(ErrorType.CHATROOM_NOT_FOUND)));
     }
 
     public ChatRoomIdResponse save(User loginUser, CreateChatRoomRequest createChatRoomRequest) {
         Optional<ChatRoom> existingChatRoom = chatRoomRepository.findChatRoomByUsers(loginUser.getId(), createChatRoomRequest.getToUserId());
         if (existingChatRoom.isPresent()) {
-            return ChatRoomIdResponse.from(existingChatRoom.orElseThrow().getId());
+            return ChatRoomIdResponse.from(existingChatRoom.orElseThrow());
         }
 
         User fromUser = userRepository.findById(loginUser.getId())
@@ -39,6 +41,6 @@ public class ChatRoomService {
                 .orElseThrow(() -> new MoneyAndLoveException(ErrorType.USER_NOT_FOUND));
         ChatRoom chatRoom = chatRoomRepository.save(ChatRoom.builder().fromUser(fromUser).toUser(toUser).build());
 
-        return ChatRoomIdResponse.from(chatRoom.getId());
+        return ChatRoomIdResponse.from(chatRoom);
     }
 }
--- Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/chat/presentation/ChattingController.java
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/chat/presentation/ChattingController.java
@@ -2,24 +2,31 @@ package com.ssafy.moneyandlove.chat.presentation;
 
 import java.util.List;
 
-import com.ssafy.moneyandlove.chat.application.ChatRoomService;
-import com.ssafy.moneyandlove.chat.domain.ChatMessage;
-import com.ssafy.moneyandlove.chat.dto.ChatMessageRequest;
-import com.ssafy.moneyandlove.chat.dto.ChatRoomIdResponse;
-import com.ssafy.moneyandlove.chat.dto.CreateChatRoomRequest;
-import com.ssafy.moneyandlove.chat.repository.ChatMessageRepository;
-import com.ssafy.moneyandlove.common.annotation.LoginUser;
-import com.ssafy.moneyandlove.user.domain.User;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.moneyandlove.chat.application.ChatRoomService;
+import com.ssafy.moneyandlove.chat.domain.ChatMessage;
+import com.ssafy.moneyandlove.chat.dto.ChatMessageRequest;
+import com.ssafy.moneyandlove.chat.dto.ChatMessageResponse;
+import com.ssafy.moneyandlove.chat.dto.ChatRoomIdResponse;
+import com.ssafy.moneyandlove.chat.dto.CreateChatRoomRequest;
+import com.ssafy.moneyandlove.chat.repository.ChatMessageRepository;
+import com.ssafy.moneyandlove.common.annotation.LoginUser;
+import com.ssafy.moneyandlove.user.domain.User;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/chat")
@@ -34,9 +41,9 @@ public class ChattingController {
     @MessageMapping("/send/{chatRoomId}")
     public void chat(@DestinationVariable String chatRoomId, @LoginUser User loginUser, @Payload ChatMessageRequest chatMessageRequest) {
         log.info("{}", loginUser.getId());
-        ChatMessage chatMessage = chatMessageRepository.save(ChatMessage.of(loginUser.getId(), chatMessageRequest));
+        ChatMessage chatMessage = chatMessageRepository.save(chatMessageRequest.toChatMessage(loginUser.getId()));
         log.info("{}",chatMessage);
-        simpMessagingTemplate.convertAndSend("/chat/receive/" + chatRoomId, chatMessage.toChatMessageResponse());
+        simpMessagingTemplate.convertAndSend("/chat/receive/" + chatRoomId, ChatMessageResponse.from(chatMessage));
     }
 
     @GetMapping("/room")
