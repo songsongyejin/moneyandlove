b8067adb3d52e17a833bae35862aa41f031a1ab8
Lee9Bin <imagen33@daum.net>
2024-08-04 20:10:20 +0900

    feat: 카카오 api 요청 에러 처리

--- Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/common/GlobalControllerAdvice.java
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/common/GlobalControllerAdvice.java
@@ -1,18 +1,29 @@
 package com.ssafy.moneyandlove.common;
 
-import com.ssafy.moneyandlove.common.error.ErrorResponse;
-import com.ssafy.moneyandlove.common.error.ErrorType;
-import com.ssafy.moneyandlove.common.exception.MoneyAndLoveException;
-
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.moneyandlove.common.error.ErrorResponse;
+import com.ssafy.moneyandlove.common.error.ErrorType;
+import com.ssafy.moneyandlove.common.exception.MoneyAndLoveException;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalControllerAdvice {
+
+	private final ObjectMapper objectMapper;
 
 	@ExceptionHandler(MoneyAndLoveException.class)
 	public ResponseEntity<ErrorResponse> handleCoreApiException(MoneyAndLoveException e) {
@@ -28,5 +39,23 @@ public class GlobalControllerAdvice {
 	public ResponseEntity<ErrorResponse> handleException(Exception e) {
 		log.error("Exception : {}", e.getMessage(), e);
 		return new ResponseEntity<>(ErrorResponse.of(ErrorType.DEFAULT_ERROR), ErrorType.DEFAULT_ERROR.getStatus());
+	}
+
+	@ExceptionHandler(HttpClientErrorException.class)
+	public ResponseEntity<Map<String, String>> handleHttpClientErrorException(HttpClientErrorException ex) {
+		Map<String, String> response = new HashMap<>();
+
+		try {
+			JsonNode jsonNode = objectMapper.readTree(ex.getResponseBodyAsString());
+			response.put("error_code", jsonNode.path("error_code").asText());
+			response.put("error", jsonNode.path("error").asText());
+			response.put("error_description", jsonNode.path("error_description").asText());
+		} catch (IOException e) {
+			response.put("error", "JSON_PARSE_ERROR");
+			response.put("error_description", e.getMessage());
+			response.put("error_code", "500");
+		}
+
+		return new ResponseEntity<>(response, ex.getStatusCode());
 	}
 }
