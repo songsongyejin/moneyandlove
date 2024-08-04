1493dc735a44f4d7db914b5cc1de9856139535b5
fasongsong <yaejin5195@naver.com>
2024-08-05 00:27:47 +0900

    feat: response 타입 생성

---
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/matching/dto/MatchingUserResponse.java
@@ -0,0 +1,30 @@
+package com.ssafy.moneyandlove.matching.dto;
+
+import com.ssafy.moneyandlove.user.domain.Gender;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Builder
+public class MatchingUserResponse {
+    private Long userId;
+    private String nickname;
+    private String profileURL;
+    private Integer age;
+    private String gender;
+    private String firstPosition;
+    private String matchingMode;
+
+    public MatchingUserResponse(Long userId, String nickname, String profileURL, Integer age, Gender gender, String firstPosition, String matchingMode) {
+        this.userId = userId;
+        this.nickname = nickname;
+        this.profileURL = profileURL;
+        this.age = age;
+        this.gender = gender != null ? gender.name() : null;
+        this.firstPosition = firstPosition;
+        this.matchingMode = matchingMode;
+    }
+
+}
