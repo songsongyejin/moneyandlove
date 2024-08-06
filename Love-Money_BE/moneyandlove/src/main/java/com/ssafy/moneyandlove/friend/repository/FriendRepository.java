857da7b6d1075fd0f26ee5ad759a913e87e29e4c
Lee9Bin <imagen33@daum.net>
2024-08-06 16:04:57 +0900

    refactor: 친구목록 조회 시 채팅방 이름도 함께 응답

--- Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/friend/repository/FriendRepository.java
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/friend/repository/FriendRepository.java
@@ -13,10 +13,11 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
 	void deleteByFollowerIdAndFollowingId(Long followerId, Long followingId);
 	void deleteByFollowingIdAndFollowerId(Long followingId, Long followerId);
 
-	@Query("SELECT new com.ssafy.moneyandlove.friend.dto.FriendInfoResponse(f.follower.id, f.follower.nickname, f.follower.age, f.follower.gender, f.follower.profileURL) " +
-		"FROM Friend f WHERE f.following.id = :followingId")
+	@Query("SELECT new com.ssafy.moneyandlove.friend.dto.FriendInfoResponse(f.follower.id, f.follower.nickname, f.follower.age, f.follower.gender, f.follower.profileURL, cr.id) " +
+		"FROM Friend f " +
+		"LEFT JOIN ChatRoom cr ON (f.follower.id = cr.fromUser.id AND f.following.id = cr.toUser.id) OR (f.follower.id = cr.toUser.id AND f.following.id = cr.fromUser.id) " +
+		"WHERE f.following.id = :followingId")
 	List<FriendInfoResponse> findFriendInfoByFollowingId(@Param("followingId") Long followingId);
-
 
 	@Query("SELECT COUNT(f) > 0 FROM Friend f WHERE f.follower.id = :followerId AND f.following.id = :followingId")
 	boolean existsByFollowingAndFollower(@Param("followingId") Long followingId, @Param("followerId") Long followerId);
