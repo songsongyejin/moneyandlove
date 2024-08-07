fc85c932f3c9c79b9a1619a0462fa3f39d8ba89f
fasongsong <yaejin5195@naver.com>
2024-08-08 00:09:37 +0900

    refactor: userId -> requestparam이 아닌 token에서 가져오게 변경

--- Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/face/presentation/FaceController.java
+++ Love-Money_BE/moneyandlove/src/main/java/com/ssafy/moneyandlove/face/presentation/FaceController.java
@@ -2,13 +2,11 @@ package com.ssafy.moneyandlove.face.presentation;
 
 import java.util.Map;
 
+import com.ssafy.moneyandlove.common.annotation.LoginUser;
+import com.ssafy.moneyandlove.user.domain.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ssafy.moneyandlove.face.application.FaceService;
 
@@ -22,8 +20,11 @@ public class FaceController {
 	private final FaceService faceService;
 
 	@GetMapping
-	public ResponseEntity<Map<String, String>> getPresignedUrl(@RequestParam Long userId, @RequestParam String prefix, @RequestParam String imageName) {
-		Map<String, String> presignedUrl = faceService.getPresignedUrl( prefix, imageName, userId);
+	public ResponseEntity<Map<String, String>> getPresignedUrl(@LoginUser User loginUser, @RequestParam String prefix, @RequestParam String imageName) {
+		Map<String, String> presignedUrl = faceService.getPresignedUrl( prefix, imageName, loginUser.getId());
 		return new ResponseEntity<>(presignedUrl, HttpStatus.OK);
 	}
+
+
+
 }
