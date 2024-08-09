2e1aed4ef85bf57cf37770efd62ec73cdeeb0edd
김성윤 <atomyoun@gmail.com>
2024-08-09 10:58:26 +0900

    feat: 환경변수 설정

--- Love-Money_FE/src/components/room/ChatBox.tsx
+++ Love-Money_FE/src/components/room/ChatBox.tsx
@@ -26,59 +26,62 @@ const ChatBox = ({
   };
 
   return (
-    <div
-      style={{ fontFamily: "DungGeunMo" }}
-      className={`${mode === "chat" ? "" : "hidden"} w-96 flex-col rounded-2xl`}
-    >
+    <div className="absolute h-screen w-screen">
       <img src={aiBot} alt="" className="absolute bottom-5 left-5 w-24" />
       <div className="absolute bottom-20 left-28 rounded-e-2xl rounded-tl-2xl border-4 border-solid border-custom-purple-color bg-white p-3 text-lg font-semibold text-custom-purple-color">
         자신이 러브헌터임을 어필해주세요!
       </div>
-      <header className="bg-chatRoom-high h-14 rounded-t-2xl bg-custom-purple-color"></header>
-      <div className="h-5/6 overflow-y-auto border border-gray-300 bg-chatRoom-color p-2">
-        {messages.map((msg, i) => {
-          const isMyMessage = msg.user === myUserName;
-          return (
-            <div
-              key={i}
-              className={`mb-3 flex ${isMyMessage ? "justify-end" : "justify-start"}`}
-            >
-              {isMyMessage ? (
-                <>
-                  <div className="speech-bubble2 mr-2 p-2">{msg.text}</div>
-                  <p className="my-auto text-2xl">{msg.Emoji}</p>
-                </>
-              ) : (
-                <>
-                  <div>
-                    <p className="ml-10 text-sm"> {msg.user}</p>
-                    <div className="flex">
+      <div className="h-full">
+        <div
+          style={{ fontFamily: "DungGeunMo" }}
+          className={`${mode === "chat" ? "" : "hidden"} mx-auto h-full w-[600px] flex-col items-center rounded-2xl pt-5`}
+        >
+          <div className="h-5/6 overflow-y-auto border border-gray-300 bg-transparent p-2">
+            {messages.map((msg, i) => {
+              const isMyMessage = msg.user === myUserName;
+              return (
+                <div
+                  key={i}
+                  className={`mb-3 flex ${isMyMessage ? "justify-end" : "justify-start"}`}
+                >
+                  {isMyMessage ? (
+                    <>
+                      <div className="speech-bubble2 mr-2 p-2">{msg.text}</div>
                       <p className="my-auto text-2xl">{msg.Emoji}</p>
-                      <div className="speech-bubble ml-2 p-2">
-                        <strong></strong> {msg.text}
+                    </>
+                  ) : (
+                    <>
+                      <div>
+                        <p className="ml-10 text-sm"> {msg.user}</p>
+                        <div className="flex">
+                          <p className="my-auto text-2xl">{msg.Emoji}</p>
+                          <div className="speech-bubble ml-2 p-2">
+                            <strong></strong> {msg.text}
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                </>
-              )}
+                    </>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+          <div className="mt-3 flex items-center rounded-b-2xl bg-chat-color p-4">
+            <input
+              type="text"
+              className="flex-grow rounded-s-lg p-2 focus:outline-custom-purple-color"
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+              placeholder="메시지를 입력하세요"
+            />
+            <div className="rounded-e-lg bg-custom-purple-color p-2 px-4">
+              <FiSend
+                className="cursor-pointer text-2xl text-white"
+                onClick={sendMessage}
+              />
             </div>
-          );
-        })}
-      </div>
-      <div className="flex items-center rounded-b-2xl bg-chat-color p-4">
-        <input
-          type="text"
-          className="flex-grow rounded-s-lg p-2 focus:outline-custom-purple-color"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          onKeyDown={handleKeyDown}
-          placeholder="메시지를 입력하세요"
-        />
-        <div className="rounded-e-lg bg-custom-purple-color p-2 px-4">
-          <FiSend
-            className="cursor-pointer text-2xl text-white"
-            onClick={sendMessage}
-          />
+          </div>
         </div>
       </div>
     </div>
--- Love-Money_FE/src/components/room/GameView.tsx
+++ Love-Money_FE/src/components/room/GameView.tsx
@@ -37,7 +37,7 @@ const GameView = ({
   return (
     <div
       id="session"
-      className={`${mode === "chat" ? "justify-end" : "justify-center"} flex h-screen p-4`}
+      className={`${mode === "chat" ? "justify-end" : "justify-center"} flex h-screen`}
     >
       {mainStreamManager && (
         <div
@@ -64,7 +64,7 @@ const GameView = ({
       )}
       <button
         onClick={() => setIsModalOpen(true)}
-        className={`${mode === "chat" ? "" : "hidden"} rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700`}
+        className={`${mode === "chat" ? "" : "hidden"} z-50 rounded text-white`}
       >
         Open Face Chat Agreement
       </button>
