9173962c4d36b458ad86ece910724aca6546b8aa
오현진 <ohj220@naver.com>
2024-08-11 03:25:50 +0900

    asfasf

--- Love-Money_FE/src/components/whats-it-to-ya/game-elements/InitialZone.tsx
+++ Love-Money_FE/src/components/whats-it-to-ya/game-elements/InitialZone.tsx
@@ -18,10 +18,7 @@ const InitialZone: React.FC<InitialZoneProps> = ({ id, cards, onDrop }) => {
   }));
 
   return cards.length > 0 ? (
-    <div
-      ref={drop}
-      className="initial-zone-container absolute bottom-5 right-5"
-    >
+    <div ref={drop} className="initial-zone-container fixed bottom-3 right-1">
       <div className="initial-zone-cards flex flex-row space-x-1 rounded-lg bg-opacity-80 px-1 py-1 shadow-lg">
         {cards.map((card) => (
           <div key={card.id} className="initial-card">
