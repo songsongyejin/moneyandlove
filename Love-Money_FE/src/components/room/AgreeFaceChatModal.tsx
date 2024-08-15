8e4dca34f176e6ee741a5a9b1fc8add389552cd7
김성윤 <atomyoun@gmail.com>
2024-08-15 21:04:14 +0900

    mainUI수정

--- Love-Money_FE/src/components/room/AgreeFaceChatModal.tsx
+++ Love-Money_FE/src/components/room/AgreeFaceChatModal.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import { IoClose } from "react-icons/io5";
+import close from "../../assets/close.png";
 
 // 공통모달창
 interface AgreeFaceChatModalProps {
@@ -24,8 +25,17 @@ const AgreeFaceChatModal: React.FC<AgreeFaceChatModalProps> = ({
       style={{ fontFamily: "DungGeunMo" }}
       className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
     >
+      {/* 모달창 닫기버튼 */}
+      <div className="fade-in-bottom mb-64 flex items-center justify-end">
+        <button
+          className="flex items-center justify-center text-white"
+          onClick={onClose}
+        >
+          <img src={close} alt="" className="w-8" />
+        </button>
+      </div>
       {/* 모달창 배경 */}
-      <div className="flex h-[500px] w-[900px] flex-col justify-center overflow-hidden rounded-[20px]">
+      <div className="fade-in-bottom flex h-[500px] w-[900px] flex-col justify-center overflow-hidden rounded-[20px]">
         <div className="flex w-full max-w-4xl flex-col items-center justify-center overflow-y-auto p-6 text-center text-xl text-white">
           {children}
         </div>
--- Love-Money_FE/src/components/room/GameView.tsx
+++ Love-Money_FE/src/components/room/GameView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import UserVideoComponent from "./UserVideoComponent";
 import AgreeFaceChatModal from "./AgreeFaceChatModal";
 import ChatBox from "./ChatBox";
@@ -10,7 +10,7 @@ import { userToken, userInfo, UserInfo } from "../../atom/store";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { updateGamePoints } from "../../utils/updateGamePoints";
 import coffee from "../../assets/coffee.png";
-// 게임 뷰 컴포넌트
+
 const GameView = ({
   mode,
   setMode,
@@ -27,72 +27,40 @@ const GameView = ({
   session,
   matchData,
 }: {
-  mode: string; // 모드 상태 변수 (채팅 또는 화상 채팅)
-  setMode: React.Dispatch<React.SetStateAction<string>>; // 모드 변경 핸들러
-  mainStreamManager: StreamManager | undefined; // 메인 스트림 관리자
-  subscriber: StreamManager | undefined; // 구독자 스트림 관리자
-  messages: { user: string; text: string; Emoji: string }[]; // 메시지 상태 변수
-  newMessage: string; // 새 메시지 상태 변수
-  setNewMessage: React.Dispatch<React.SetStateAction<string>>; // 새 메시지 변경 핸들러
-  sendMessage: () => void; // 메시지 전송 함수
-  leaveSession: () => void; // 세션 떠나기 함수
-  isModalOpen: boolean; // 모달 상태 변수
-  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>; // 모달 상태 변경 핸들러
+  mode: string;
+  setMode: React.Dispatch<React.SetStateAction<string>>;
+  mainStreamManager: StreamManager | undefined;
+  subscriber: StreamManager | undefined;
+  messages: { user: string; text: string; Emoji: string }[];
+  newMessage: string;
+  setNewMessage: React.Dispatch<React.SetStateAction<string>>;
+  sendMessage: () => void;
+  leaveSession: () => void;
+  isModalOpen: boolean;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
   myUserName: string;
-  session: Session; // 추가: OpenVidu 세션 객체
+  session: Session;
   matchData: any;
 }) => {
   const navigate = useNavigate();
   const token = useRecoilValue(userToken);
   const setUserInfo = useSetRecoilState(userInfo);
-
-  // 포인트 복구 함수
-  const restorePoints = async () => {
-    if (token && matchData) {
-      try {
-        console.log("포인트 복구 중...");
-        // 복구할 포인트 결정 (deductPoints에서 차감한 포인트와 동일한 값을 사용)
-        let gamePoint = 100; // 기본 복구 포인트
-
-        switch (matchData.fromUser.matchingMode) {
-          case "random":
-            gamePoint = 100;
-            break;
-          case "love":
-            gamePoint = 500;
-            break;
-          case "top30":
-            gamePoint = 1000;
-            break;
-          default:
-            console.warn(
-              "알 수 없는 매칭 모드:",
-              matchData.fromUser.matchingMode
-            );
-        }
-
-        await updateGamePoints({ gamePoint, token });
-
-        // Recoil 상태 업데이트
-        setUserInfo((prevUserInfo: UserInfo | null) => {
-          if (prevUserInfo) {
-            return {
-              ...prevUserInfo,
-              gamePoint: prevUserInfo.gamePoint + gamePoint, // 포인트 복구
-            };
-          }
-          return prevUserInfo;
-        });
-
-        console.log(`${gamePoint} 포인트 복구 완료`);
-      } catch (error) {
-        console.error("포인트 복구 실패", error);
-      }
-    }
-  };
-
+  const [firstModal, setFirstModal] = useState(true);
   const [hasLeft, setHasLeft] = useState(false);
   const [isNormalExit, setIsNormalExit] = useState(false);
+
+  // 버튼이 나타날 시간을 관리하는 상태
+  const [showNextStepButton, setShowNextStepButton] = useState(false);
+
+  useEffect(() => {
+    // 15초 후에 버튼을 보여줌
+    const timer = setTimeout(() => {
+      setShowNextStepButton(true);
+    }, 15000);
+
+    // 컴포넌트 언마운트 시 타이머 정리
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     if (session) {
@@ -112,13 +80,11 @@ const GameView = ({
     }
   }, [session, hasLeft]);
 
-  // 상대방이 보낸 "정상 종료" 신호를 받았을 때 처리하는 로직
   useEffect(() => {
     if (session) {
       const handleUserLeaving = (event: any) => {
         const data = JSON.parse(event.data);
         if (data.userId === matchData.toUser.userId) {
-          // 상대방이 정상적으로 나가는 경우
           console.log("상대방이 정상적으로 세션을 종료했습니다.");
           setIsNormalExit(true);
           setHasLeft(true);
@@ -139,9 +105,50 @@ const GameView = ({
     navigate("/main");
   };
 
+  const restorePoints = async () => {
+    if (token && matchData) {
+      try {
+        console.log("포인트 복구 중...");
+        let gamePoint = 100;
+
+        switch (matchData.fromUser.matchingMode) {
+          case "random":
+            gamePoint = 100;
+            break;
+          case "love":
+            gamePoint = 500;
+            break;
+          case "top30":
+            gamePoint = 1000;
+            break;
+          default:
+            console.warn(
+              "알 수 없는 매칭 모드:",
+              matchData.fromUser.matchingMode
+            );
+        }
+
+        await updateGamePoints({ gamePoint, token });
+
+        setUserInfo((prevUserInfo: UserInfo | null) => {
+          if (prevUserInfo) {
+            return {
+              ...prevUserInfo,
+              gamePoint: prevUserInfo.gamePoint + gamePoint,
+            };
+          }
+          return prevUserInfo;
+        });
+
+        console.log(`${gamePoint} 포인트 복구 완료`);
+      } catch (error) {
+        console.error("포인트 복구 실패", error);
+      }
+    }
+  };
+
   const renderChatMode = () => (
     <>
-      {/* 채팅 모드일 때의 UI */}
       <ChatBox
         myUserName={myUserName}
         mode={mode}
@@ -152,38 +159,27 @@ const GameView = ({
         matchData={matchData}
       />
 
-      <button
-        onClick={() => setIsModalOpen(true)}
-        className="shake-left absolute top-1/2 z-50 mr-10 flex items-center rounded bg-transparent p-4 text-2xl font-bold text-white hover:scale-110"
-        style={{ fontFamily: "DungGeunMo" }}
-      >
-        <img src={coffee} alt="" className="w-16" />
-        <p>다음 단계로 넘어가기!</p>
-      </button>
+      {showNextStepButton && (
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="shake-left absolute top-1/2 z-50 mr-10 flex items-center rounded bg-transparent p-4 text-2xl font-bold text-white hover:scale-110"
+          style={{ fontFamily: "DungGeunMo" }}
+        >
+          <img src={coffee} alt="" className="w-16" />
+          <p>다음 단계로 넘어가기!</p>
+        </button>
+      )}
     </>
   );
 
-  {
-    /* 화상 채팅 모드일 때의 UI */
-  }
   const renderFaceChatMode = () => (
-    // <div
-    //   className="absolute inset-0 bg-cover"
-    //   style={{
-    //     backgroundImage: `url(${CafeBackground})`,
-    //     backgroundPosition: "center bottom",
-    //   }}
-    // >
     <div className="relative h-full w-full">
-      {/* 배경 이미지 */}
-      {/* 빠른 배경 이미지 렌더링 위해 img 태그 사용 */}
       <img
         src={CafeBackground}
         alt="Cafe Background"
         className="absolute inset-0 h-full w-full object-cover"
         style={{ objectPosition: "center bottom" }}
       />
-      {/* 본인 캠화면 (왼쪽하단에 위치) */}
       {mainStreamManager && (
         <div
           id="main-video"
@@ -196,7 +192,6 @@ const GameView = ({
           <UserVideoComponent streamManager={mainStreamManager} />
         </div>
       )}
-      {/* 상대방 캠화면 (가운데 상단에 위치) */}
       {subscriber && (
         <div
           id="video-container"
@@ -215,7 +210,6 @@ const GameView = ({
         </div>
       )}
 
-      {/* WhatsItToYa 게임 화면 */}
       <div className="absolute inset-0 z-50 flex items-center justify-center">
         <WhatsItToYa
           session={session}
@@ -234,7 +228,24 @@ const GameView = ({
       }`}
     >
       {mode === "chat" ? renderChatMode() : renderFaceChatMode()}
-
+      <AgreeFaceChatModal
+        isOpen={firstModal}
+        onClose={() => setFirstModal(false)}
+        title={""}
+      >
+        <p className="mb-4">서로 얼굴을 보기 전, 가볍게 진심을 나눠보세요.</p>
+        <p className="mb-4">
+          얼굴을 가리면 감정은 더 잘 숨겨지겠지만, 표정리더기가 당신의 얼굴을
+          읽어낼 것입니다.
+        </p>
+        <p className="mb-4">
+          만약 상대를 속이고 싶다면, 표정 연기를 더욱 잘해야겠죠?
+        </p>
+        <p className="mb-4">
+          가벼운 이모티콘 채팅이지만, 이 안에 담긴 진심이 중요한 순간입니다.
+        </p>
+        <p> 자신이 Love 임을 어필해보세요.</p>
+      </AgreeFaceChatModal>
       <AgreeFaceChatModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
