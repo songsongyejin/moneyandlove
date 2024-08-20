6e727168b8cb04cdb6d04c5b5055e4f8a2c471fb
Lee9Bin <imagen33@daum.net>
2024-08-20 22:50:34 +0900

    final commit

---
+++ webrtc/src/main/java/com/ssafy/webRTC/WebRtcApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.webRTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRtcApplication.class, args);
+	}
+
+}
---
+++ webrtc/src/test/java/com/ssafy/webRTC/WebRtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.webRTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
