2aa0c8b8fa4f93cfaabbec149f0e100be7ed7d54
Lee9Bin <imagen33@daum.net>
2025-02-26 17:59:18 +0900

    feat: LoginUserArgumentResolver 통합
    기존 ChattingUserArgumentResolver 제거
    LoginUserArgumentResolver가 HTTP와 WebSocket 모두에서 @LoginUser 어노테이션으로 사용자 정보를 주입하도록 상속 및 구현
    WebSocket 요청은 STOMP 헤더의 사용자 정보를 활용하여 인증 정보를 제공

--- backend/src/main/java/com/ssafy/moneyandlove/common/resolver/LoginUserArgumentResolver.java
+++ backend/src/main/java/com/ssafy/moneyandlove/common/resolver/LoginUserArgumentResolver.java
@@ -1,6 +1,10 @@
 package com.ssafy.moneyandlove.common.resolver;
 
+import java.util.Objects;
+
 import org.springframework.core.MethodParameter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -16,7 +20,8 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver,
+	org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver {
 
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
@@ -30,5 +35,11 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 		//SecurityContext에서 값 꺼내기
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		return authentication.getPrincipal();
+	}
+
+	@Override
+	public Object resolveArgument(MethodParameter parameter, Message<?> message) {
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+		return ((Authentication)Objects.requireNonNull(accessor.getUser())).getPrincipal();
 	}
 }
